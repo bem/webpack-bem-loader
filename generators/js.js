f5490c0db931dd2896cadd4fbcbf11d876ee9e80
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-01-24 15:40:14 +0300

    Add i18n generator and i18n-core.

---
+++ generators/js.js
@@ -0,0 +1,15 @@
+
+/**
+ * @param {BemFile[]} files
+ * @returns {String}
+ */
+module.exports = function generateJsStr(files) {
+    return files
+        .reduce((acc, file, i) => acc.concat(
+            i !== files.length - 1
+                ? `require('${file.path}'),`
+                : `(require('${file.path}').default || require('${file.path}')).applyDecls()`
+        ), ['('])
+        .concat(')')
+        .join('\n');
+}
