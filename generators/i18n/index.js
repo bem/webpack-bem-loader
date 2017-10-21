4265091ba2625e117d33424597d1eb6524082c6b
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-10-21 17:37:34 +0300

    Add REACT_APP_BEM_LANG as alias to BEM_LANG

--- generators/i18n/index.js
+++ generators/i18n/index.js
@@ -1,20 +1,40 @@
-const path = require('path'),
-    requiredPath = require('required-path');
+const path = require('path');
+const requiredPath = require('required-path');
+
+const strLang = (file, lang) => `
+if (
+    process.env.BEM_LANG?
+        process.env.BEM_LANG === '${lang}' :
+        process.env.REACT_APP_BEM_LANG?
+            process.env.REACT_APP_BEM_LANG === '${lang}' :
+            'en' === '${lang}'
+) {
+    return core()
+        .decl(require('${requiredPath(path.join(file.path, lang))}'))
+        ('${file.cell.entity.id}');
+}
+`;
+
+const langEnvErr = (env, langs) => `process.env.${env} && `
+    + `console.error('No match of process.env.${env} { ' + process.env.${env} + ' } in provided langs: { ${langs.join(', ')} }');`;
+
+const onlyDEV = strings => `
+if (process.env.NODE_ENV === 'development') {
+    ${strings.join('\n')}
+}
+`;
 
 function generateI18n(langs, files) {
-    const strLang = (file, lang) => `
-        if (process.env.BEM_LANG ? process.env.BEM_LANG === '${lang}' : 'en' === '${lang}') {
-            return core()
-                .decl(require('${requiredPath(path.join(file.path, lang))}'))
-                    ('${file.cell.entity.id}');
-        }`;
 
     return files
         .reduce((acc, file) => {
             return acc.concat(langs.map(lang => strLang(file, lang)));
         }, ['(function() {', `var core = require('${requiredPath(path.join(__dirname, "core"))}');`])
         .concat([
-            `console.error('No match of process.env.BEM_LANG {' + process.env.BEM_LANG + '} in provided langs: {${langs}}');`,
+            onlyDEV([
+                langEnvErr('BEM_LANG', langs),
+                langEnvErr('REACT_APP_BEM_LANG', langs)
+            ]),
             'return function(){};\n})()'
         ])
         .join('\n');
